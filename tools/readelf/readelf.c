797b3ec776757813001c17798df560e720565a79
张政 <21371155@buaa.edu.cn>
2023-03-07 20:55:10 +0800

    ex1.1 demo'

--- tools/readelf/readelf.c
+++ tools/readelf/readelf.c
@@ -43,26 +43,18 @@ int readelf(const void *binary, size_t size) {
 	Elf32_Half sh_entry_count;
 	Elf32_Half sh_entry_size;
 	/* Exercise 1.1: Your code here. (1/2) */
-	sh_table = binary;
-	sh_table+=16*sizeof(unsigned char);
-	sh_table += 2*sizeof(Elf32_Half);
-	sh_table += sizeof(Elf32_Word);
-	sh_table+=sizeof(Elf32_Addr);
-	sh_table+=sizeof(Elf32_Off);
-	binary+=*(Elf32_Off *)sh_table;
-	sh_table+=sizeof(Elf32_Off);
-	sh_table+=sizeof(Elf32_Word);
-	sh_table+=3*sizeof(Elf32_Half);
-	sh_entry_size=*(Elf32_Half *)sh_table;
-	sh_entry_count=*(Elf32_Half *)(sh_table+sizeof(Elf32_Half));
+	sh_table = ehdr;
+	sh_table += ehdr -> e_shoff;
+	sh_entry_size = ehdr -> e_shentsize;
+	sh_entry_count = ehdr -> e_shnum;
 		// For each section header, output its index and the section address.
 		// The index should start from 0.
 		for (int i = 0; i < sh_entry_count; i++) {
 			const Elf32_Shdr *shdr;
 			unsigned int addr;
 			/* Exercise 1.1: Your code here. (2/2) */
-			shdr=(Elf32_Shdr*)(binary+i*sh_entry_size);
-			addr=*(unsigned int *)shdr;
+			shdr=(Elf32_Shdr*)(sh_table+i*sh_entry_size);
+			addr=shdr->sh_addr;
 			printf("%d:0x%x\n", i, addr);
 		}
 
